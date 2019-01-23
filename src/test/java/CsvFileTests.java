a1be8cbd4628143416679a16406dab3005b8d28b
rportela <rodrigo.portela@hotmail.com>
2019-01-23 17:26:48 -0200

    Update CsvFileTests.java

--- src/test/java/CsvFileTests.java
+++ src/test/java/CsvFileTests.java
@@ -1,7 +1,9 @@
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import org.junit.Assert;
@@ -20,6 +22,7 @@ public class CsvFileTests {
 
 	@Test
 	public void testIsin() throws IOException {
+
 		IsinFile isin = new BovespaParser().parseIsin(getClass().getResourceAsStream("isinp.zip"));
 
 		List<IsinFileEmissor> emissores = isin.getEmissores();
@@ -41,5 +44,27 @@ public class CsvFileTests {
 		List<Negs> negs = new BovespaDataSource(localCache).fetchNegsMercadoAVista(date);
 		Assert.assertTrue(negs != null);
 		Assert.assertTrue(!negs.isEmpty());
+	}
+
+	@Test
+	public void testIsinForDuplicateCnpj() throws IOException {
+
+		final IsinFile isin = new BovespaParser().parseIsin(getClass().getResourceAsStream("isinp.zip"));
+		final HashMap<Long, List<IsinFileEmissor>> cnpjMap = new HashMap<>();
+		isin.getEmissores().stream().forEach(emissor -> {
+			if (emissor.cnpj != null && emissor.cnpj > 0) {
+				cnpjMap
+						.computeIfAbsent(emissor.cnpj, f -> new ArrayList<>())
+						.add(emissor);
+			}
+		});
+
+		cnpjMap.entrySet().stream().filter(e -> e.getValue().size() > 1).forEach(e -> {
+			System.out.println("______________________");
+			System.out.println("Duplicates of " + e.getKey());
+			for (IsinFileEmissor emissor : e.getValue()) {
+				System.out.println(emissor.nome + " (" + emissor.codigo + ")");
+			}
+		});
 	}
 }
